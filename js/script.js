84129efa90de2c8de8c21dc1176139f03a823021
victorgileau <vgileau@hotmail.com>
2025-04-25 18:24:09 -0400

    ajouter le menu fonctionnel

--- js/script.js
+++ js/script.js
@@ -1,33 +1,83 @@
 const tabAllOption = [
     {
         expresion: "an",
-        partieMotUn: "ans",
-        partieMotDeux: "s",
-        partieMotTroix: "",
+        partieMotUn: "",
+        partieMotDeux: "an",
+        partieMotTroix: "s",
         audioSourceExpresion: "./medias/audio/lego_test-sound.mp3",
         audioSourceMots: "./medias/audio/lego_test-sound.mp3",
         imgSource: "./medias/img/photojeu/test/annee-v2.jpg",
     },
     {
         expresion: "test",
-        partieMotUn: "test",
-        partieMotDeux: "encore",
-        partieMotTroix: "",
+        partieMotUn: "",
+        partieMotDeux: "test",
+        partieMotTroix: "s",
         audioSourceExpresion: "./medias/audio/lego_test-sound.mp3",
         audioSourceMots: "./medias/audio/lego_test-sound.mp3",
         imgSource: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
     },
+    {
+        expresion: "test2",
+        partieMotUn: "a",
+        partieMotDeux: "test",
+        partieMotTroix: "s2",
+        audioSourceExpresion: "./medias/audio/lego_test-sound.mp3",
+        audioSourceMots: "./medias/audio/lego_test-sound.mp3",
+        imgSource: "https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg",
+    },
 ];
 
-let audioBtn = document.querySelectorAll('.audioBtn');
 const nextBtn = document.querySelector('.next');
 numberOfClick = 0;
 numberOfClickSecond = 0;
-/*rempresente q quelle étape l'utilisateur est rendu*/
+/*rempresente a quelle étape l'utilisateur est rendu*/
 whereYouAre = 0;
-
 restartWithNew(0, nextBtn);
 
+/* Menu */
+const menu = document.querySelector('.sideMenu');
+const contentMenu = document.querySelector('.contentMenu');
+const sideMenuCheck = document.querySelector('#sideMenuCheck');
+const btnMenu = document.querySelector('.btnMenu');
+
+/* function pour verifier ou est l'utilisateur*/
+function checkOption(color) {
+    let newOptions = document.querySelectorAll('.option');
+    newOptions.forEach((el, index) => {
+        if (index == whereYouAre) {
+            el.style.backgroundColor = color;
+        } else {
+            el.style.backgroundColor = '';
+        }
+    });
+}
+
+/* Créé les options dans le menu */
+tabAllOption.forEach((el) => {
+    console.log('test ' + el.expresion);
+    contentMenu.innerHTML += `
+    <div class="option">
+    ${el.expresion}
+    </div>
+    `;
+});
+
+/* Ajoute des évenement au options du menu */
+let options = document.querySelectorAll('.option');
+options.forEach((el, index) => {
+    if (index == whereYouAre) {
+        el.style.backgroundColor = '#2a4c44';
+    }
+    el.addEventListener('click', () => {
+        whereYouAre = index;
+        restartWithNew(index, nextBtn);
+        checkOption('#2a4c44');
+    });
+});
+
+
+/*Fuction pour aller a une nouvelle option + son déroulement*/
 function restartWithNew(number, btnToContinue) {
     let content = document.querySelector('.content');
     numberOfClick = 0;
@@ -46,7 +96,7 @@ function restartWithNew(number, btnToContinue) {
     </div>
     <div class="textContent textContent--2">
         <button class="answer hidden">Réponse</button>
-        <h3 class="hidden"><em>${tabAllOption[number].partieMotUn}</em>${tabAllOption[number].partieMotDeux}</h3>
+        <h3 class="hidden">${tabAllOption[number].partieMotUn}<em>${tabAllOption[number].partieMotDeux}</em>${tabAllOption[number].partieMotTroix}</h3>
         <button class="audioBtn hidden">
             <svg viewBox="0 0 343 266" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9 160.5V106C9 103.239 11.2386 101 14 101H72.8315C74.2149 101 75.5365 100.427 76.4819 99.4168L124.85 47.7419C127.948 44.4312 133.5 46.6241 133.5 51.1587V209.792C133.5 214.194 128.222 216.448 125.042 213.404L76.4501 166.888C75.5195 165.997 74.2809 165.5 72.9926 165.5H14C11.2386 165.5 9 163.261 9 160.5Z" stroke="black" stroke-width="18"/>
@@ -66,7 +116,9 @@ function restartWithNew(number, btnToContinue) {
     let img = document.querySelector('.img');
     img.style.backgroundImage = `url(${tabAllOption[number].imgSource})`;
 
+    /* Bouttons d'audio */
     let newAudioBtn = document.querySelectorAll('.audioBtn');
+
     newAudioBtn[0].addEventListener('click', () => {
         let audioTestOne = new Audio(`${tabAllOption[number].audioSourceExpresion}`);
         audioTestOne.volume = 0.2;
@@ -87,6 +139,7 @@ function restartWithNew(number, btnToContinue) {
         numberOfClickSecond++;
     });
 
+    /* Boutton réponse */
     answerBtn.addEventListener('click', () => {
         let hidden = document.querySelectorAll('.hidden');
         hidden.forEach((el) => {
@@ -97,6 +150,7 @@ function restartWithNew(number, btnToContinue) {
         answerBtn.classList.add('hidden');
     });
     
+    /* Boutton récompense */
     rewardBtn.addEventListener('click', () => {
         let img = document.querySelector('.img');
         rewardBtn.classList.add('hidden');
@@ -109,6 +163,8 @@ function restartWithNew(number, btnToContinue) {
 
         btnToContinue.classList.remove('hidden');
     });
+
+    checkOption('#2a4c44');
 }
 
 /*Evenement enclancher a chaque click sur le bouton "next"*/
@@ -120,6 +176,7 @@ nextBtn.addEventListener('click', () => {
         restartWithNew(whereYouAre, nextBtn);
     } else {
         /*Recommence du début*/
+        whereYouAre = 0;
         restartWithNew(0, nextBtn);
     }
 });
