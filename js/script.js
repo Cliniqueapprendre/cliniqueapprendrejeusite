d37ec959503ae071219c748db9f59c8c840826a3
Khatymiss <khalytia@hotmail.ca>
2025-05-06 14:50:28 -0400

    switch to the bubble appearing, not word

--- js/script.js
+++ js/script.js
@@ -1146,7 +1146,6 @@ btnDownlad.addEventListener('click', () => {
 /* function pour aller en "fullScreen" */
 function activateAndDeactivateFullScreen(element) {
     if (isFullScreenOn == false) {
-        // get out full screen
         btnFullScreen.style.backgroundImage = 'url(medias/img/medias_ui/modefenetre.png)';
         isFullScreenOn = true;
         document.documentElement.requestFullscreen().catch((err) => {
@@ -1156,7 +1155,6 @@ function activateAndDeactivateFullScreen(element) {
         console.log('Full Screen : ' + isFullScreenOn);
         
     } else if (isFullScreenOn == true) {
-        // full screen
         btnFullScreen.style.backgroundImage = 'url(medias/img/medias_ui/pleinecran.png)';
         document.exitFullscreen();
         isFullScreenOn = false;
@@ -1360,7 +1358,7 @@ if (localStorage.getItem('whereYouAre') != null) {
     restartWithNew(whereYouAre, nextBtn);
 }
 
-/* Va ala possiont de L'option selectionner quand l'utilisateur hover sur le menu pour la rpemière fois */
+/* Va a la possiont de L'option selectionner quand l'utilisateur hover sur le menu pour la rpemière fois */
 let menuSide = document.querySelector('.sideMenu');
 let HasMouseHoverdMenu = false;
 menuSide.addEventListener('mouseover', () => {
@@ -1372,7 +1370,7 @@ menuSide.addEventListener('mouseover', () => {
 })
 
 
-/*Fuction pour aller a une nouvelle option + son déroulement*/
+/*Function pour aller a une nouvelle option + son déroulement*/
 function restartWithNew(number, btnToContinue) {
     let isAnswerActive = false;
     let isImageBtnActive = false;
@@ -1382,28 +1380,15 @@ function restartWithNew(number, btnToContinue) {
     content.innerHTML = `
     <div class="textContent textContent--1">
         <h1>${tabAllOption[number].expresion}</h1>
-        <button class="audioBtn">
-            <svg viewBox="0 0 343 266" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 160.5V106C9 103.239 11.2386 101 14 101H72.8315C74.2149 101 75.5365 100.427 76.4819 99.4168L124.85 47.7419C127.948 44.4312 133.5 46.6241 133.5 51.1587V209.792C133.5 214.194 128.222 216.448 125.042 213.404L76.4501 166.888C75.5195 165.997 74.2809 165.5 72.9926 165.5H14C11.2386 165.5 9 163.261 9 160.5Z" stroke="black" stroke-width="18"/>
-                <path d="M171 179C171 179 206 171.45 206 131.712C206 91.9738 171 88 171 88" stroke="black" stroke-width="18" stroke-linecap="round"/>
-                <path d="M215 222C215 222 266.5 198.5 266.5 131.5C266.5 64.5 215 40 215 40" stroke="black" stroke-width="18" stroke-linecap="round"/>
-                <path d="M260 257C260 257 331.5 221 333 129.5C334.5 38 260 9 260 9" stroke="black" stroke-width="18" stroke-linecap="round"/>
-                </svg>                    
-        </button>
+        <button class="audioBtn"></button> 
         <!--<button class="readText">
             Lire (robot)
         </button>-->
     </div>
-    <div class="textContent textContent--2">
-        <button class="answer hidden"></button>
-        <h3 class="textAnswer hidden">${tabAllOption[number].partieMotUn}<em>${tabAllOption[number].partieMotDeux}</em>${tabAllOption[number].partieMotTroix}</h3>
+    <button class="answer hidden"></button> 
+    <div class="textContent textContent--2 hidden">
+        <h3 class="textAnswer">${tabAllOption[number].partieMotUn}<em>${tabAllOption[number].partieMotDeux}</em>${tabAllOption[number].partieMotTroix}</h3>
         <button class="audioBtn audioBtn--answer hidden">
-            <svg viewBox="0 0 343 266" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M9 160.5V106C9 103.239 11.2386 101 14 101H72.8315C74.2149 101 75.5365 100.427 76.4819 99.4168L124.85 47.7419C127.948 44.4312 133.5 46.6241 133.5 51.1587V209.792C133.5 214.194 128.222 216.448 125.042 213.404L76.4501 166.888C75.5195 165.997 74.2809 165.5 72.9926 165.5H14C11.2386 165.5 9 163.261 9 160.5Z" stroke="black" stroke-width="18"/>
-                <path d="M171 179C171 179 206 171.45 206 131.712C206 91.9738 171 88 171 88" stroke="black" stroke-width="18" stroke-linecap="round"/>
-                <path d="M215 222C215 222 266.5 198.5 266.5 131.5C266.5 64.5 215 40 215 40" stroke="black" stroke-width="18" stroke-linecap="round"/>
-                <path d="M260 257C260 257 331.5 221 333 129.5C334.5 38 260 9 260 9" stroke="black" stroke-width="18" stroke-linecap="round"/>
-                </svg> 
         </button>
         <!--<button class="readText hidden">
             Lire (robot)
@@ -1465,7 +1450,7 @@ function restartWithNew(number, btnToContinue) {
 
     /* Boutton réponse (mot) */
     answerBtn.addEventListener('click', () => {
-        let textAnswer = document.querySelector('.textAnswer');
+        let textAnswer = document.querySelector('.textContent--2');
 
         textAnswer.classList.remove('hidden');
         newAudioBtn[1].classList.remove('hidden');
